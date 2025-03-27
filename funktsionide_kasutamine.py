f504501e2aad1ea639ededf53f2c788d61ed2c25
opilane <opilane@HP-CZC2349HTB>
2025-03-27 10:56:48 +0200

    1

--- funktsionide_kasutamine.py
+++ funktsionide_kasutamine.py
@@ -1,18 +1,126 @@
 
 from kasutajate_funks_Melikov import *
-
-a=float(input("Arv1: "))
-b=float(input("Arv2: "))
-t=input("Tehe: ")
+##1
+while True:
+   try:
+        a=float(input("Arv1: "))
+        if a.is_integer():
+            break
+   except:
+       print("Sisesta arv! ")
+while True:
+    try:
+        b=float(input("Arv2: "))
+        if b.is_integer():
+            break
+    except:
+        print("Sisesta arv! ")
+while True:
+    try:
+        t=input("Tehe: ")
+        if not t.isnumeric():
+            break
+    except:
+        print("Sisesta tehe! ")
 vastus=arithmetic(a,b,t)
 print(vastus)
 
-
+#2
 aasta=int(input("Mis aassta tahad kontrollida? "))
 if aasta>0:
     if is_year_leap(aasta):
         print(f"{aasta} on liigaaasta")
     else:
         print(f"{aasta} ei ole liigaasta")
+
+#3
+while True:
+    try:
+        a=float(input("Ruudu külg= "))
+        if a >0 and a.is_integer():
+            break
+        else:
+            print("Sisesta positiivse arvu! ")
+    except:
+        print("Sisesta positiivse arvu! ")
+P,S,d=square(a)
+print(P,S,d)
+
+
+##4
+while True:
+    try:
+        kuu=int(input("Sisesta kuu numbri, kui tahad teada saada mis hooajas ta on "))
+        if kuu in range(1,13):
+            h=season(kuu)
+            break
+    except:
+        print("Kuud on vahemikus 1-12")
+print(f"{kuu}. kuu on hooajas nimega {h}")
+
+
+#5
+
+while True:
+    try:
+        euro=float(input("Sisesta kui palku eurot paned pangi "))
+        if euro>0:
+            break
+    except:
+        print("Sisesta positiivse arvu ")
+while True:
+    try:
+        aastad=int(input("Sisesta paljuks aastaks paned raha pangi "))
+        if aastad>0:
+            break
+    except:
+        print("Sisesta aastade arv!")
+vastus=bank(euro,aastad)
+print(f"Saad {vastus} eurot")
+
+##6 
+while True:
+    try:
+        arv=int(input("Sisesta arv 0-1000 "))
+        prime=is_prime(arv)
+        if arv in range(0,1001):
+            break
+    except:
+        print("Sisesta arv 0-1000")
+if prime==True:
+    print(f"{arv} on lihtne")
+else:
+    print(f"{arv} ei ole lihtne")
+
+#7
+while True:
+    try:
+        päev=int(input("Sisesta päev "))
+        if päev in range(1,32):
+            
+            break
+    except:
+        print("Päevad on 1-31")
+while True:
+    try:
+        kuu=int(input("Sisesta kuu "))
+        if kuu in range(1,13):
+            
+            break
+    except:
+        print("Kuud on 1-12")
+while True:
+    try:
+        aasta=int(input("Sisesta aasta "))
+        if aasta >0:
+            break
+    except:
+        print("Aastad algavad 1st")
+eks=date(päev,kuu,aasta)
+if eks==True:
+    print(f"Kuupäev {päev}. {kuu}. {aasta}. eksisteerib")
+else:
+    print(f"Kuupäev {päev}. {kuu}. {aasta}. ei eksisteeri")
+
 
 
--- kasutajate_funks_Melikov.py
+++ kasutajate_funks_Melikov.py
@@ -1,3 +1,4 @@
+from math import *
 def arithmetic(arv1:float,arv2:float,tehe:str)-> any:
     """Funktsioon töötab nagu lihtne kalkulaator
     + - liitmine
@@ -30,3 +31,98 @@ def is_year_leap(year:int)-> bool:
     else:
         vastus=False
     return vastus
+
+def square(square_side:float)-> any:
+    """Arvutab ruudu ümbermõõt,läbimõõt ja diagonaal
+
+    :param float square_side: Sisend ujukomaarvu tüübina
+    :rtype: var funktsioon tagastab kolm arvu: P, S, d
+    """
+
+    if square_side>0:
+        P=square_side*4
+        S=square_side**2
+        d=square_side*sqrt(2)
+    else:
+        print("Arv peab olema positiivne")
+    return P,S,d
+    return S
+    return d
+
+def season(month:int)-> str:
+    """Kirjutab millele hooajale kulub sisestatud kuu
+    
+    :param int month: Sisend 1-12 täisarvu tüübina
+    :rtype: str funktsioon tagastab vastust tekstina
+    """
+    talv=[12,1,2]
+    kevad=[3,4,5]
+    suvi=[6,7,8]
+    sügis=[9,10,11]
+    if month in talv:
+        vastus="Talv"
+    elif month in kevad:
+        vastus="Kevad"
+    elif month in suvi:
+        vastus="Suvi"
+    elif month in sügis:
+        vastus="Sügis"
+    else:
+        print("Viga!")
+    return vastus
+
+def bank(summ:float,years:int)->float:
+    """Kirjutab palju raha saad pangist
+
+    :param float summ: palju eurot pangasse
+    :param int years: paljuks aastaks paned raha
+    :rtype: float funktsioon tagastab palju raha lõppuks saad
+    """
+ 
+    for aasta in range(years):
+   
+
+        summ*=1.1
+    return summ
+        
+
+def is_prime(arv:int)->bool:
+    """Tagastab True, kui arv on lihtne
+    False, kui mitte
+    :param int arv: arv mida tahad kontrollida
+    :rtype: bool funktsioon tagastab True, kui arv on lihtne
+    """
+ 
+    if arv <=1:
+        return False
+    for i in range(2,arv):
+        if arv%i==0:
+            return False
+        return True
+           
+
+def date(day:int,month:int,year:int)->bool:
+    """Funktsioon kontrollib kas on sisestatud kuupäev meie kalendris või mitte
+    :param int day: päev täisarv tüübina
+    :param int month: kuu täisarv tüübina
+    :param int year: aasta täisarv tüübina
+    :rtype: bool funktsioon tagastab True, kui sisestatud kuupäev eksisteerib ja False, kui ei eksisteeri
+    """
+    v=False
+    if month in range(1,13) and year >0:
+        if month in [1,3,5,7,8,10,12]:
+            if day in range(1,32):
+                v=True
+        if month in [4,6,9,11]:
+            if day in range(1,31):
+                v=True
+        if month==2:
+            if is_year_leap(year):
+                if day in range(1,30):
+                    v=True
+            elif not is_year_leap(year):
+                if day in range(1,29):
+                    v=True
+       
+    return v
+                
